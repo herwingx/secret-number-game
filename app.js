32be34c329c7b21df5e3a3456d79e797245126a0
Eduardo Macías <herwingmacias@gmail.com>
2024-03-02 12:40:06 -0600

    Add changes

--- app.js
+++ app.js
@@ -6,10 +6,61 @@
 
   $text.innerHTML = 'Ingresa un numero del 1 al 10'
  */
+let maximosIntentos = 3;
+let intentos = 0;
+let secretNumber = 0;
 
-  function setTextElement(element, text) {
-    let elementHTML = document.querySelector(element);
-    elementHTML.innerHTML = text;
+
+function setTextElement(element, text) {
+  let elementHTML = document.querySelector(element);
+  elementHTML.innerHTML = text;
+}
+
+function generateSecretNumber() {
+  return Math.floor(Math.random() * 10 + 1);
+}
+
+function checkIntent() {
+  let userNumber = parseInt(document.getElementById("user-value").value);
+  if (userNumber == secretNumber) {
+    //Acertamos, fue verdadera la condición
+    setTextElement("p", `Acertaste, el número es: ${userNumber}`);
+    document.getElementById("restart").removeAttribute("disabled");
+    intentos == 1
+      ? setTextElement("p", `Lo lograste en ${intentos} intento`)
+      : setTextElement("p", `Lo lograste en ${intentos} intentos`);
+  } else {
+    userNumber > secretNumber
+      ? setTextElement("p", "El numero secreto es menor ")
+      : setTextElement("p", "El numero secreto es mayor");
+    intentos++;
   }
 
-  
+  if (intentos > maximosIntentos) {
+    setTextElement(
+      "p",
+      `Lo siento llegaste al numero maximo de ${maximosIntentos}`
+    );
+  }
+  cleanBox();
+}
+
+function initialConditions() {
+  setTextElement("h1", "Juego del numero secreto");
+  setTextElement("p", "Indica un número del 1 al 10: ");
+  secretNumber = generateSecretNumber();
+  intentos = 1;
+}
+
+function cleanBox() {
+  document.getElementById("user-value").value = "";
+}
+
+function restartGame() {
+  cleanBox();
+  initialConditions();
+  document.getElementById("restart").setAttribute("disabled", true);
+}
+
+initialConditions();
+console.log(secretNumber);
