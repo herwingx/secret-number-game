5af7b9880386db9078a003ca20d1791e4e0a5bff
Eduardo Macías <herwingmacias@gmail.com>
2024-03-05 09:47:11 -0600

    Add changes

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-/* let $title = document.querySelector('h1');
-
-  $title.innerHTML = 'Juego del número secreto'
-
-  let $text = document.querySelector('p');
-
-  $text.innerHTML = 'Ingresa un numero del 1 al maxNumber'
- */
 let maximosIntentos = 3;
 let intentos = 0;
 let secretNumber = 0;
-let ListRandomNumber = [];
+let listRandomNumber = [];
 let maxNumber = 10;
 
 function setTextElement(element, text) {
@@ -17,35 +9,52 @@ function setTextElement(element, text) {
   elementHTML.innerHTML = text;
 }
 
+function newGame() {
+  let startGame = document.getElementById("start");
+  startGame.style.display = "none";
+  let restartGame = document.getElementById("restart");
+  restartGame.style.display = "none";
+  let newGame = document.getElementById("new-game");
+  newGame.style.display = "block";
+}
+
 function generateSecretNumber() {
   let numberGenerate = Math.floor(Math.random() * maxNumber + 1);
-  console.log(ListRandomNumber);
-  console.log(numberGenerate);
-  if (ListRandomNumber.length == maxNumber) {
+  if (listRandomNumber.length == maxNumber) {
     setTextElement("p", "Ya se sortearon todo los numeros disponibles");
+    document.getElementById("user-value").setAttribute("disabled", true)
+    newGame();
+
+  // Refrescar cada 5 segundos (5000 milisegundos)
+  
   } else {
-    if (ListRandomNumber.includes(numberGenerate)) {
+    if (listRandomNumber.includes(numberGenerate)) {
       return generateSecretNumber();
     } else {
-      ListRandomNumber.push(numberGenerate);
+      listRandomNumber.push(numberGenerate);
       return numberGenerate;
     }
   }
 }
+
 function checkIntent() {
   let userNumber = parseInt(document.getElementById("user-value").value);
-  if (userNumber == secretNumber) {
-    //Acertamos, fue verdadera la condición
-    setTextElement("p", `Acertaste, el número es: ${userNumber}`);
-    document.getElementById("restart").removeAttribute("disabled");
-    intentos == 1
-      ? setTextElement("p", `Lo lograste en ${intentos} intento`)
-      : setTextElement("p", `Lo lograste en ${intentos} intentos`);
+  if (document.getElementById("user-value").value.trim() == "") {
+    setTextElement("p", `Porfavor ingresa un numero para continuar`);
   } else {
-    userNumber > secretNumber
-      ? setTextElement("p", "El numero secreto es menor ")
-      : setTextElement("p", "El numero secreto es mayor");
-    intentos++;
+    if (userNumber == secretNumber) {
+      //Acertamos, fue verdadera la condición
+      intentos == 1
+        ? setTextElement("p", `Lo lograste en ${intentos} intento`)
+        : setTextElement("p", `Lo lograste en ${intentos} intentos`);
+      document.getElementById("start").setAttribute("disabled", true);
+      document.getElementById("restart").removeAttribute("disabled");
+    } else {
+      userNumber > secretNumber
+        ? setTextElement("p", "El numero secreto es menor ")
+        : setTextElement("p", "El numero secreto es mayor");
+      intentos++;
+    }
   }
 
   if (intentos > maximosIntentos) {
@@ -53,6 +62,8 @@ function checkIntent() {
       "p",
       `Lo siento llegaste al numero maximo de ${maximosIntentos}`
     );
+    document.getElementById("start").setAttribute("disabled", true);
+    document.getElementById("restart").removeAttribute("disabled");
   }
   cleanBox();
 }
@@ -61,6 +72,9 @@ function initialConditions() {
   setTextElement("h1", "Juego del numero secreto");
   setTextElement("p", `Indica un número del 1 al ${maxNumber}: `);
   secretNumber = generateSecretNumber();
+  document.getElementById("start").removeAttribute("disabled");
+  document.getElementById("restart").setAttribute("disabled", true);
+  console.log(secretNumber);
   intentos = 1;
 }
 
@@ -71,8 +85,6 @@ function cleanBox() {
 function restartGame() {
   cleanBox();
   initialConditions();
-  document.getElementById("restart").setAttribute("disabled", true);
 }
 
 initialConditions();
-console.log(secretNumber);
